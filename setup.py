8008fc23064ee40d82d556973c625bae21361f6a
whiteavian <whiteavian@users.noreply.github.com>
2017-07-24 19:21:47 -0700

    Rename the sql_database folder to data_compare and update the setup.py script appropriately.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='SQLDatabase',
     version='0.1dev',
-    packages=['sql_database',],
+    packages=['data_compare',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
     install_requires=[
