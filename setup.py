4652fc60485f438caa0602dc71c8c07956c4b462
lwa <whiteavian@noreply.github.com>
2017-09-26 15:36:53 -0400

    CNF: import correct setup and gitignore swp and eggs.

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from distutils.core import setup
-
-# Install with pip install .
+from setuptools import setup
 
 setup(
     name='SQLDatabase',
     version='0.1dev',
-    packages=['data_compare',],
+    py_modules=['data_compare',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
     install_requires=[
         'sqlalchemy',
+        'sqlalchemy-utils',
     ],
 )
