5021cbaa0af2dc47365f3259574f9ec33ba97e9b
Laura White-Avian <whiteavian@noreply.github.com>
2017-09-27 16:39:48 -0400

    CFG: add mysql to setup.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
     install_requires=[
+        'MySQL-python',
         'nose',
         'nose-pudb',
         'sqlalchemy',
