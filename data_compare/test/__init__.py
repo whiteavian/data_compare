9855b68de4e3e263d5ce96448dc45a433cda44b5
Laura White-Avian <whiteavian@users.noreply.github.com>
2017-10-04 15:38:19 -0400

    BUG: fix cleanup after system error (pairing with eqfinney)

--- data_compare/test/__init__.py
+++ data_compare/test/__init__.py
@@ -13,27 +13,29 @@ class SQLDatabaseTestCase (TestCase):
     HOST = 'localhost'
 
     def setUp(self):
-        CONN_STR_SUFFIX = time()
+        try:
+            CONN_STR_SUFFIX = time()
 
-        PG_CONN_STR = "postgresql+psycopg2://{}:{}@{}:5432/pgdb_{}{{}}".format(
-                            self.DB_USER, self.DB_PASS, self.HOST, CONN_STR_SUFFIX)
-        MY_CONN_STR = "mysql+mysqldb://{}:{}@{}/mydb_{}{{}}".format(
-                            self.DB_USER, self.DB_PASS, self.HOST, CONN_STR_SUFFIX)
-        # Because of the way pyodbc requires odbc.ini, maybe this setup will not work. 
-        # Maybe we should use pymssql driver instead? TODO
-        # MS_CONN_STR = "mssql+pyodbc://{}:{}@?driver=SQL+Server+Native+Client+11.0".format(self.DB_USER, self.DB_PASS)
-        LITE_CONN_STR = "sqlite:///test{}{{}}.db".format(CONN_STR_SUFFIX)
+            PG_CONN_STR = "postgresql+psycopg2://{}:{}@{}:5432/pgdb_{}{{}}".format(
+                                self.DB_USER, self.DB_PASS, self.HOST, CONN_STR_SUFFIX)
+            MY_CONN_STR = "mysql+mysqldb://{}:{}@{}/mydb_{}{{}}".format(
+                                self.DB_USER, self.DB_PASS, self.HOST, CONN_STR_SUFFIX)
+            # Because of the way pyodbc requires odbc.ini, maybe this setup will not work.
+            # Maybe we should use pymssql driver instead? TODO
+            # MS_CONN_STR = "mssql+pyodbc://{}:{}@?driver=SQL+Server+Native+Client+11.0".format(self.DB_USER, self.DB_PASS)
+            LITE_CONN_STR = "sqlite:///test{}{{}}.db".format(CONN_STR_SUFFIX)
 
-        self.dbs = {
-            'lite_a': LITE_CONN_STR.format("a"),
-            'lite_b': LITE_CONN_STR.format("b"),
-            'pg_a': PG_CONN_STR.format("a"),
-            'pg_b': PG_CONN_STR.format("b"),
-            'my_a': MY_CONN_STR.format("a"),
-            'my_b': MY_CONN_STR.format("b"),
-            # MS_CONN_STR,
-        }
-        map(create_database, self.dbs.values())
+            self.dbs = {
+                'lite_a': LITE_CONN_STR.format("a"),
+                'lite_b': LITE_CONN_STR.format("b"),
+                'pg_a': PG_CONN_STR.format("a"),
+                'pg_b': PG_CONN_STR.format("b"),
+                'my_a': MY_CONN_STR.format("a"),
+                'my_b': MY_CONN_STR.format("b"),
+                # MS_CONN_STR,
+            }
+            map(create_database, self.dbs.values())
 
         # Ensure test databases are ephemeral.
-        atexit.register(map, drop_database, self.dbs.values())
+        finally:
+            atexit.register(map, drop_database, self.dbs.values())
--- data_compare/test/test_sql_database.py
+++ data_compare/test/test_sql_database.py
@@ -1,7 +1,38 @@
+from data_compare.sql_database import SQLDatabase
 from . import SQLDatabaseTestCase
 
 
 class TestSQLDatabase (SQLDatabaseTestCase):
 
-    def test_thing(self):
-        pass
+    def test_compare_a_b(self):
+        a = SQLDatabase(self.dbs['pg_a'])
+        b = SQLDatabase(self.dbs['pg_b'])
+
+        a.compare_schemas(b)
+
+        table_a = a.table_from_name('users')
+        table_b = b.table_from_name('users')
+
+        res_a = a.conn.execute(table_a.select())
+        res_b = b.conn.execute(table_b.select())
+
+        a = res_a.fetchall()
+        b = res_b.fetchall()
+
+        headers_a = tuple(c.name for c in table_a.columns)
+        headers_b = tuple(c.name for c in table_b.columns)
+
+        # Add error in "real" case for when the length > 1
+        pk_a = a.table_pk_col_names(table_a)[0]
+        pk_b = b.table_pk_col_names(table_b)[0]
+
+        a.insert(0, headers_a)
+        b.insert(0, headers_b)
+
+        a
+        b
+
+        a_rd = RelationalData(a, pk_a)
+        b_rd = RelationalData(b, pk_b)
+        a_rd.compare(b_rd)
+        a_rd.errors
