247911efb0940b493a96bb5d14b8cb032f8f43c7
Laura White-Avian <whiteavian@users.noreply.github.com>
2017-10-04 11:11:14 -0400

    REF, TST, DOC: update TestRelationalData

--- data_compare/test/test_relational_data.py
+++ data_compare/test/test_relational_data.py
@@ -5,8 +5,9 @@ from . import DataCompareTestCase
 class TestRelationalData (DataCompareTestCase):
 
     def setUp(self):
+        self.headers = ('id', 'col1', 'col2')
         self.rd = RelationalData(
-            [('id', 'col1', 'col2'),
+            [self.headers,
              (1, 'foo', 'loo'),
              (2, 'out', 'ina'),
              (3, 'go', 'return'),
@@ -15,7 +16,7 @@ class TestRelationalData (DataCompareTestCase):
             'id')
 
         self.rd_comp = RelationalData(
-            [('id', 'col1', 'col2'),
+            [self.headers,
              (1, 'oof', 'loo'),
              (2, 'out', 'eee'),
              (3, 'go', 'return'),
@@ -28,10 +29,15 @@ class TestRelationalData (DataCompareTestCase):
         self.rd.comparand = self.rd_comp
 
     def test_val(self):
-        assert self.rd.val((4, 'ooo', 'ppp'), 'col1') == 'ooo'
+        """Test that an arbitrary value is appropriately extracted from a row."""
+        row = (4, 'ooo', 'ppp')
+        assert self.rd.val(row, self.headers[1]) == row[1]
 
     def test_pkey_val(self):
-        assert self.rd.pkey_val((2, 'out', 'ina')) == 2
+        """Test that the primary key is appropriately extracted from a row."""
+        row = (2, 'out', 'ina')
+        pkey_index = self.headers.index(self.rd.pkey)
+        assert self.rd.pkey_val(row) == row[pkey_index]
 
     def test_matching_row(self):
         res, i = self.rd.matching_row(1, 1)
