7f68c4cd5822df098655a7b219e86d13bdfb1565
Laura White-Avian <whiteavian@users.noreply.github.com>
2017-10-05 11:13:44 -0400

    SCP: move one off scripts into their own folder

--- scripts/one-offs/test_compare_schemas.py
+++ scripts/one-offs/test_compare_schemas.py
@@ -1,5 +1,5 @@
-from model_a_test import BaseA
-from model_b_test import BaseB
+from data_compare.test.model_a_test import BaseA
+from data_compare.test.model_b_test import BaseB
 from sqlalchemy import create_engine
 
 
@@ -17,15 +17,14 @@ DBB_CONN = DB_CONN_FORMAT.format(database='db_b', **DB_CONFIG_DICT)
 
 
 def test_compare_schemas():
-    pass
-    # engine_a = create_engine(DB_CONN_FORMAT.format(
-    #     database='db_a', **DB_CONFIG_DICT))
-    #
-    # engine_b = create_engine(DB_CONN_FORMAT.format(
-    #     database='db_b', **DB_CONFIG_DICT))
-    #
-    # BaseA.metadata.create_all(engine_a)
-    # BaseB.metadata.create_all(engine_b)
+    engine_a = create_engine(DB_CONN_FORMAT.format(
+        database='db_a', **DB_CONFIG_DICT))
+
+    engine_b = create_engine(DB_CONN_FORMAT.format(
+        database='db_b', **DB_CONFIG_DICT))
+
+    BaseA.metadata.create_all(engine_a)
+    BaseB.metadata.create_all(engine_b)
 
     # create sqlite, postgres, mysql, maria? and mssql databases and exhaustively
     # compare them. Start with sqlite for now, and later extend.
