ed70d8df1179ab604cf9059038773425e42da92a
Laura White-Avian <whiteavian@users.noreply.github.com>
2017-10-20 00:53:39 -0400

    DOC, REF: add docstrings throughout SQLDatabase, remove obsolete empty update method, and rename rd.

--- data_compare/sql_database.py
+++ data_compare/sql_database.py
@@ -87,21 +87,26 @@ class SQLDatabase (object):
 
     @property
     def tables(self):
+        """Return the table objects of all tables in this database."""
         return self.metadata.tables
 
     @property
     def table_names(self):
+        """Return the names of the tables in this database (or database schema)."""
         return self.inspector.get_table_names(schema=self.schema)
 
     def table_pk_col_names(self, table):
+        """Return the primary key column names for the given table."""
         return [c.name for c in table.columns if c.primary_key]
 
     def table_from_name(self, table_name):
+        """Return the table object from the table name."""
         if self.schema:
             table_name = self.schema + table_name
         return self.tables[table_name]
 
     def column_from_table(self, table, column_name):
+        """Return the column object from a table and column name."""
         if isinstance(table, str):
             table = self.table_from_name(table)
         return table.columns[column_name]
@@ -132,10 +137,14 @@ class SQLDatabase (object):
         map(self.compare_table_schemas, common_table_names)
 
     def dual_set_compare(self, a, b, diff_key=None):
+        """Compare both directions of differences between two sets-like objects.
+
+        TODO give a concrete example of how this is used."""
         self.single_set_compare(a, b, 'a', diff_key)
         self.single_set_compare(b, a, 'b', diff_key)
 
     def single_set_compare(self, a, b, prefix, diff_key):
+        """Compare a single direction of two set-like objects."""
         for i in remove_ignore(set(a) - set(b)):
             if diff_key:
                 self.differences[diff_key]['{}_{}'.format(i, prefix)] = {}
@@ -156,6 +165,7 @@ class SQLDatabase (object):
         self.compare_table_columns(ta, tb)
 
     def compare_table_columns(self, ta, tb):
+        """Record the columns present in one table and not the other."""
         ta_col_names = set(col.name for col in ta.columns)
         tb_col_names = set(col.name for col in tb.columns)
 
@@ -171,12 +181,15 @@ class SQLDatabase (object):
 
             self.differences[ta.name][col_name] = compare(COLUMN_KEYS, col_a, col_b)
 
-    def table_rd(self, table):
+    def relational_data_for_table(self, table):
+        """Return a RelationalData object for the given table."""
         data = self.session.query(table).all()
         headers = [tuple(c.name for c in table.columns)]
         pks = self.table_pk_col_names(table)
+
         assert len(pks) == 1, \
             "Compare data only works with data having exactly one primary key column."
+
         return RelationalData(headers + data, pks[0])
 
     def compare_data(self):
@@ -193,17 +206,20 @@ class SQLDatabase (object):
         self.data_diffs = defaultdict(dict)
 
         for table in self.tables.values():
-            rd = self.table_rd(table)
+            data = self.relational_data_for_table(table)
             comparand_table = self.comparand.table_from_name(table.name)
-            rd.compare(self.comparand.table_rd(comparand_table))
-            self.data_diffs[table.name] = rd.errors
+            data.compare(self.comparand.relational_data_for_table(comparand_table))
+            self.data_diffs[table.name] = data.errors
 
         return self.data_diffs
 
     def update_data_to_match_comparand(self):
+        """Insert, remove, and update data to match the comparand."""
         data_diffs = self.compare_data()
         sorted_table_names = [t.name for t in self.metadata.sorted_tables if t.name in data_diffs.keys()]
 
+        # Due to foreign key constraints, we must iterate twice through the tables.
+        # Iterate in forward order so inserts do not violate fk constraints.
         for table_name in sorted_table_names:
             table = self.table_from_name(table_name)
 
@@ -212,6 +228,8 @@ class SQLDatabase (object):
             self.add_missing_rows(table, active_diffs[COMPARAND_MISSING_ROWS].keys())
             self.update_changed_values(table, active_diffs[CHANGED_ROWS])
 
+        # Iterate through the tables in reversed order so row deletion does not
+        # violate foreign key constraints.
         for table_name in reversed(sorted_table_names):
             table = self.table_from_name(table_name)
 
@@ -250,6 +268,7 @@ class SQLDatabase (object):
                     values({col: changed_columns[col][1]}).where(table.c.id == id))
 
     def delete_missing_rows(self, table, rows):
+        """Remove rows that are present in this db but missing from the comparand."""
         for row in rows:
             # TODO don't forget that assuming the pk is the first item isn't a valid
             # assumption. Definitely refactor all of this.
@@ -264,18 +283,7 @@ class SQLDatabase (object):
     def update_schema_b_from_a(self):
         """Update the schema of db_b to that found in db_a."""
         pass
-    
-    def update_data_b_from_a(self):
-        """Update the data in db_b to that found in db_a.
-    
-        This function assumes that the schemas are equivalent enough
-        that this data update will work. If it will not, the function
-        rolls back the transaction (if the db_b is of a database type
-        that supports rollback) and returns an exception. This function 
-        also assumes write privileges on db_b.
-        """
-        pass
-    
+
     def update_b_sequences_from_a(self):
         pass
     
